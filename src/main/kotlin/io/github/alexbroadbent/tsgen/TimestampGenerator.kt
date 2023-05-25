1be642707a8ea65f855f4cdd480c128a56e26dbc
owpk <voyzvz@gmail.com>
2023-05-25 18:12:51 +0800

    MASTER: change time format

--- src/main/kotlin/io/github/alexbroadbent/tsgen/TimestampGenerator.kt
+++ src/main/kotlin/io/github/alexbroadbent/tsgen/TimestampGenerator.kt
@@ -2,11 +2,17 @@ package io.github.alexbroadbent.tsgen
 
 import io.github.alexbroadbent.tsgen.config.TimestampGeneratorSettings
 import java.time.Instant
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 object TimestampGenerator {
 
     fun generateTimestamp(
-        instant: Instant = Instant.now(),
-        settings: TimestampGeneratorSettings = TimestampGeneratorSettings.instance
-    ): String = settings.formatter().format(instant)
+            instant: Instant = Instant.now(),
+            settings: TimestampGeneratorSettings = TimestampGeneratorSettings.instance
+    ): String {
+        val currentDate = LocalDateTime.now();
+        val format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        return currentDate.format(format);
+    }
 }
